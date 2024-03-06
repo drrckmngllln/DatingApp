c011a7aea13d8ca1feed80826fd380f9d8555222
Derrick Manglallan <manglallan.derrick@gmail.com>
2024-03-06 18:16:20 +0800

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -21,7 +21,10 @@ app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
-app.UseCors();
+app.UseCors(builder => 
+{
+    builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+});
 
 app.MapControllers();
 
