9980058db5338ee74c6b1933b4674e631e916ebc
Derrick Manglallan <manglallan.derrick@gmail.com>
2024-03-28 15:56:30 +0800

    End of section 4

--- API/Program.cs
+++ API/Program.cs
@@ -1,7 +1,4 @@
-using API.Data;
-using API.Interfaces;
-using API.Services;
-using Microsoft.EntityFrameworkCore;
+using API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,26 +6,26 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-builder.Services.AddScoped<ITokenService, TokenService>();
+builder.Services.AddApplicationServices(builder.Configuration);
 
-builder.Services.AddDbContext<DataContext>(opt =>
-{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-
-builder.Services.AddCors();
+builder.Services.AddIdentityServices(builder.Configuration);
 
 var app = builder.Build();
 
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
-
-app.UseCors(builder => 
+app.UseCors(builder =>
 {
-    builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+    builder
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+        .WithOrigins("https://localhost:4200");
 });
+
+app.UseAuthentication();
+
+app.UseAuthorization();
 
 app.MapControllers();
 
