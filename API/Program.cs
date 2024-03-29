54d1d54b41b6a27ee879aa9ca30f776b523a105f
Derrick Manglallan <manglallan.derrick@gmail.com>
2024-03-29 19:29:34 +0800

    End of section 7

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 using API.Extensions;
+using API.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,8 +13,7 @@ builder.Services.AddIdentityServices(builder.Configuration);
 
 var app = builder.Build();
 
-
-app.UseHttpsRedirection();
+app.UseMiddleware<ExceptionMiddleware>();
 
 app.UseCors(builder =>
 {
