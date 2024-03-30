78bbfe3a4333ac812f2a85c606796570f12f6b40
Derrick Manglallan <manglallan.derrick@gmail.com>
2024-03-31 07:24:03 +0800

    End of section 10

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -18,5 +18,7 @@ public class AutoMapperProfiles : Profile
                 dest.Age, opt => 
                 opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
         CreateMap<Photo, PhotoDto>();
+
+        CreateMap<MemberUpdateDto, AppUser>();
     }
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -19,6 +19,9 @@ import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { ServerErrorComponent } from './errors/server-error/server-error.component';
 import { MemberCardComponent } from './members/member-card/member-card.component';
 import {JwtInterceptor} from "./_interceptors/jwt.interceptor";
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import {GalleryComponent} from "ng-gallery";
+import {LoadingInterceptor} from "./_interceptors/loading.interceptor";
 
 @NgModule({
   declarations: [
@@ -32,7 +35,8 @@ import {JwtInterceptor} from "./_interceptors/jwt.interceptor";
     TestErrorComponent,
     NotFoundComponent,
     ServerErrorComponent,
-    MemberCardComponent
+    MemberCardComponent,
+    MemberEditComponent
   ],
   imports: [
     BrowserModule,
@@ -40,11 +44,13 @@ import {JwtInterceptor} from "./_interceptors/jwt.interceptor";
     HttpClientModule,
     BrowserAnimationsModule,
     FormsModule,
-    SharedModule
+    SharedModule,
+    GalleryComponent
   ],
   providers: [
     {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true},
-    {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true}
+    {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true},
+    {provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true},
   ],
   bootstrap: [AppComponent]
 })
