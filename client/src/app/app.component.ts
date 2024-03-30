92851af7dbc33ce0bfd54eed094dc4574d35d316
Derrick Manglallan <manglallan.derrick@gmail.com>
2024-03-30 15:51:36 +0800

    End of section 9

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import {AccountService} from "./_services/account.service";
 import {User} from "./_models/user";
@@ -15,8 +16,6 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     this.setCurrentUser();
   }
-
-
 
   setCurrentUser() {
     const userString = localStorage.getItem('user');
